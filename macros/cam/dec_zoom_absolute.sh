a8a82482d79b06022a3831bc6497fb825db12147
PHoNX123 <s.attila1986@gmail.com>
2023-03-09 14:25:26 +0100

    Autocommit from 2023-03-09 14:25:26
    Klipper on commit: e6ef48c
    
    Moonraker on commit: 18f5ff4
    
    Mainsail version: v2.4.1

---
+++ macros/cam/dec_zoom_absolute.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Adjust control options for /dev/video0
+
+# Specify the control option to adjust and the step size
+ctrl="zoom_absolute"  # replace with the desired control option
+step=1            # replace with the desired step size
+
+# Get the current value of the control option
+value=$(v4l2-ctl -d /dev/video0 -C $ctrl | awk -F ":" '{print $2}')
+
+# Calculate the new value of the control option
+new_value=$((step))
+
+# Set the new value of the control option
+v4l2-ctl -d /dev/video0 -c $ctrl=$new_value
