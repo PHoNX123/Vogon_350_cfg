4ec7896eae8ab392c9228ded97d2b4352f0a3219
PHoNX123 <s.attila1986@gmail.com>
2023-03-09 10:37:06 +0100

    Autocommit from 2023-03-09 10:37:06
    Klipper on commit: e6ef48c
    
    Moonraker on commit: 18f5ff4
    
    Mainsail version: v2.4.1

---
+++ macros/increase_brightness.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# +++  brightness of /dev/video0 by 10
+
+brightness=$(v4l2-ctl -d /dev/video0 -C brightness | awk -F ":" '{print $2}')
+new_brightness=$((brightness + 10))
+v4l2-ctl -d /dev/video0 -c brightness=$new_brightness
